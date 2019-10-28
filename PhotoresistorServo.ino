fa42fc78aaf04a196e89c9086dd01025b5c2e17a
mrunciman <m.runciman@imperial.ac.uk>
2019-10-28 15:35:43 +0000

    Set up Servo, LED and LDR, then read from LDR and set Servo angle. Use micros() for timing of program.

--- PhotoresistorServo.ino
+++ PhotoresistorServo.ino
@@ -8,13 +8,13 @@ float prevServo = 77.5; // The servo limits are set arbitrarily at 15 - 150 degr
 //Setup variables for LDR and LED
 int photoPin = 0;
 float photoValue = 0;
-float desiredPhoto = 575.0;
+float desiredPhoto = 625.0;
 int photoError;
 
 //Set PID gains
-float Kp = 0.1;
+float Kp = 0.04;
 float Ki = 0.00;
-float Kd = 0.01;
+float Kd = 0.005;
 
 float totalError = 0.0;
 float prevError = 0.0;
@@ -24,6 +24,11 @@ float derivative = 0.0;
 float PID = 0.0;
 float interpPID;
 
+//Set variables for timing of program
+unsigned long period = 10000; //period in microseconds
+float samplingFreq = 0.01;
+unsigned long prevTime;
+
 void setup() {
   myservo.attach(5);
   Serial.begin(115200);
@@ -31,9 +36,10 @@ void setup() {
 
 void loop() {
   //Read LDR value when interrupt is triggered 
-  if (freqFlag == true){
+  if (micros() - prevTime >= period){
+    prevTime = prevTime + period;
     photoValue = analogRead(photoPin);
-    Serial.println(photoValue);
+    //Serial.println(photoValue);
 
     photoError = desiredPhoto - photoValue;
     //Serial.println(photoError);
@@ -41,7 +47,7 @@ void loop() {
     totalError = totalError + photoError;
     proportional = photoError * Kp;
     integral = totalError * Ki;
-    derivative = ((photoError - prevError) * reducedFreq) * Kd; // multiply by sampling freq instead of dividing by time
+    derivative = ((photoError - prevError) / samplingFreq) * Kd; // multiply by sampling freq instead of dividing by time
     prevError = photoError;
     PID = proportional + integral + derivative;
     //Serial.println(PID);
@@ -49,7 +55,7 @@ void loop() {
     interpPID = (135.0)*((PID)/1023.0); //Linear interpolation to convert PID error signal to angular position error
     //Serial.println(interpPID);
     servoValue = prevServo + interpPID;
-    Serial.println(servoValue);
+    //Serial.println(servoValue);
 
     //Keep servo value within normal boundaries
     if (servoValue > 150.0){
@@ -58,7 +64,7 @@ void loop() {
     else if (servoValue < 15.0){
       servoValue = 15.0;
     }
-    myservo.write(servoValue);
+    //myservo.write(servoValue);
     prevServo = servoValue;
   }
 }
